58a988f05fbd9f339ea86925316658511a931ebb
Clément ROYER <clementita54@hotmail.fr>
2019-02-02 18:13:10 +0100

    Less files

---
+++ SetupClient/files/input.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+## EPITECH PROJECT, 2019
+## 
+## Clément ROYER
+## clement.royer@epitech.eu
+
+source $PWD/log.sh
+echo -e "PASS $PASS\nUSER $USER_T\nNICK $USER_T"
+
+echo -e "CAP REQ :twitch.tv/membership"
+
+echo -e "JOIN #$CHANNEL"
+while read line ; do
+      echo "$line" | sed "s/^/PRIVMSG #$CHANNEL :/"
+done
---
+++ SetupClient/setup.sh
@@ -0,0 +1,7 @@
+## EPITECH PROJECT, 2019
+## 
+## Clément ROYER
+## clement.royer@epitech.eu
+
+cd files
+./main.sh | ./output.sh
