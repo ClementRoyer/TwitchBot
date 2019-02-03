f17ac2405735254a1491c6a2a4ca222299b73d30
Clément ROYER <clementita54@hotmail.fr>
2019-02-03 01:11:10 +0100

    am

--- Client/main.sh
+++
@@ -1,6 +0,0 @@
-## EPITECH PROJECT, 2019
-## 
-## Clément ROYER
-## clement.royer@epitech.eu
-
-nc irc.twitch.tv 6667 < <(./input.sh)
--- Client/setup.sh
+++
@@ -1,6 +0,0 @@
-## EPITECH PROJECT, 2019
-## 
-## Clément ROYER
-## clement.royer@epitech.eu
-
-./main.sh | ./output.sh
